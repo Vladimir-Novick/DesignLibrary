396284f098e05bdab7f205a98bbbf9e2843fb592
vladimir Novick <vlad.novick@gmail.com>
2020-10-14 15:41:26 +0300

    Encode binary data into a textual representation ( escape format )

--- src/DesignLib/CDesignUtils.cpp
+++ src/DesignLib/CDesignUtils.cpp
@@ -1,6 +1,13 @@
 #include "stdafx.h"
+#include <string>
 #include "CDesignUtils.h"
 #include "CDesignButtonAction.h"
+#include <cctype>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
 
 HBITMAP CDesignUtils::ScaleBitmapInt(HBITMAP hBitmap, WORD wNewWidth, WORD wNewHeight)
 {
@@ -31,6 +38,49 @@ HBITMAP CDesignUtils::ScaleBitmapInt(HBITMAP hBitmap, WORD wNewWidth, WORD wNewH
 	::SelectObject(destDC.m_hDC, hbmOldDest);
 
 	return hbmResult;
+}
+
+/// <summary>
+///   Encode binary data into a textual representation ( escape format )
+///   escape - converts zero bytes and high-bit-set bytes to octal sequences (\nnn) and doubles backslashes.
+/// </summary>
+/// <param name="str"></param>
+/// <param name="strLen"></param>
+/// <returns></returns>
+std::string CDesignUtils::escape_string(char* str, int strLen)
+{
+	std::stringstream stream;
+
+	stream << std::uppercase
+		<< std::oct
+		<< std::setfill('0');
+
+	char* ch = str;
+
+	for (int i = 0; i < strLen; i++)
+	{
+		int code = static_cast<unsigned char>(*ch);
+
+		if (std::isprint(code))
+		{
+			if (*ch == '\\') {
+				stream.put(*ch);
+				stream.put(*ch);
+			}
+			else {
+				stream.put(*ch);
+			}
+		}
+		else
+		{
+			stream << "\\"
+				<< std::setw(3)
+				<< code;
+		}
+		ch++;
+	}
+
+	return stream.str();
 }
 
 
--- src/DesignLib/CDesignUtils.h
+++ src/DesignLib/CDesignUtils.h
@@ -8,6 +8,7 @@ using namespace std;
 class CDesignUtils
 {
 public:
+	static string escape_string(char* str, int strLen);
 	static HBITMAP ScaleBitmapInt(HBITMAP hBitmap, WORD wNewWidth, WORD wNewHeight);
 };
 
